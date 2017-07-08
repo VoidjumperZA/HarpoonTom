74ffb9cde4c4f0c6c89c14152eb13763ca2be88e
Josh van Asten - Desktop <joshmathew.va@gmail.com>
2017-07-08 06:08:18 +0200

    + Added a basic folder structure + Added Platform.cs and PlatformSegment.cs scripts + Added an empty prefab to the Resources folder

---
+++ Harpoon Tom/Assets/Game Folder/Scripts/Platform.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour 
+{	
+	[SerializeField] private int platformWidth;
+	[SerializeField] private int platformHeight;
+	private GameObject[][] platformGrid;
+	// Use this for initialization
+	void Start () 
+	{
+		platformGrid = new GameObject[platformWidth][platformHeight]; 
+		for (int i = 0; i < platformWidth; i++) 
+		{
+			platformGrid[i] = Instantiate(Resources.Load("Resources/Prefabs/PlatformSegement"));
+			for (int j = 1; j < platformHeight; j++) 
+			{
+				platformGrid[j] = Instantiate(Resources.Load("Resources/Prefabs/PlatformSegement"));
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{		
+	}
+}
---
+++ Harpoon Tom/Assets/Game Folder/Scripts/PlatformSegment.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSegment : MonoBehaviour 
+{	
+	[Range(0, 100)] private int health;
+	private bool reinforced;
+	// Use this for initialization
+	void Start () 
+	{		
+		health = 100;
+		reinforced = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	/// <summary>
+	/// Flat out replaces existing health value
+	/// </summary>
+	/// <param name="pNewHealth">P new health.</param>
+	public void SetHealth(int pNewHealth)
+	{
+		
+	}
+
+	/// <summary>
+	/// Increments the health by given value.
+	/// </summary>
+	/// <param name="pDeltaHealth">P delta health.</param>
+	public void IncrementHealth(int pDeltaHealth)
+	{
+		
+	}
+
+	/// <summary>
+	/// Gets the health.
+	/// </summary>
+	/// <returns>The health.</returns>
+	public int GetHealth()
+	{
+		return health;
+	}
+
+	/// <summary>
+	/// Re- or Uninforces the segment.
+	/// </summary>
+	/// <param name="pState">If set to <c>true</c> p state.</param>
+	public void ReinforceSegment(bool pState)
+	{
+		reinforced = pState;
+	}
+
+	/// <summary>
+	/// Determines whether this instance is reinforced.
+	/// </summary>
+	/// <returns><c>true</c> if this instance is reinforced; otherwise, <c>false</c>.</returns>
+	public bool IsReinforced()
+	{
+		return reinforced;
+	}
+}
